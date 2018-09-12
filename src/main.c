de3fa55c6745f34c785a9d920c0f8ad0a09eda9c
Thom Overhand <thom_overhand@hotmail.com>
2018-09-12 20:29:21 +0200

    Unit test for every piece

--- src/main.c
+++ src/main.c
@@ -34,9 +34,51 @@ static char *test_knight_attacks() {
     return 0;
 }
 
+static char *test_queen_attacks() {
+    Bitboard queen_loc = 0x10000000ULL;
+    Bitboard white_pieces = 0x10005400ULL;
+		Bitboard black_pieces = 0xFF000000000000ULL;
+
+    Bitboard outcome_attacks = compute_queen(queen_loc, white_pieces, black_pieces, clear_file);
+    Bitboard expected_attacks = 0x925438EF380000ULL;
+    char *message = (char*)malloc(128 * sizeof(char));
+    sprintf(message, "outcome: %lx != expected: %lx", outcome_attacks, expected_attacks);
+    mu_assert(message, outcome_attacks == expected_attacks);
+    return 0;
+}
+
+static char *test_rook_attacks() {
+    Bitboard rook_loc = 0x10000000ULL;
+    Bitboard white_pieces = 0x10005400ULL;
+		Bitboard black_pieces = 0xFF000000000000ULL;
+
+    Bitboard outcome_attacks = compute_rook(rook_loc, white_pieces, black_pieces, clear_file);
+    Bitboard expected_attacks = 0x101010EF100000ULL;
+    char *message = (char*)malloc(128 * sizeof(char));
+    sprintf(message, "outcome: %lx != expected: %lx", outcome_attacks, expected_attacks);
+    mu_assert(message, outcome_attacks == expected_attacks);
+    return 0;
+}
+
+static char *test_bishop_attacks() {
+    Bitboard bishop_loc = 0x10000000ULL;
+    Bitboard white_pieces = 0x10005400ULL;
+		Bitboard black_pieces = 0xFF000000000000ULL;
+
+    Bitboard outcome_attacks = compute_bishop(bishop_loc, white_pieces, black_pieces, clear_file);
+    Bitboard expected_attacks = 0x82442800280000ULL;
+    char *message = (char*)malloc(128 * sizeof(char));
+    sprintf(message, "outcome: %lx != expected: %lx", outcome_attacks, expected_attacks);
+    mu_assert(message, outcome_attacks == expected_attacks);
+    return 0;
+}
+
 static char *all_tests() {
     mu_run_test(test_king_attacks);
     mu_run_test(test_knight_attacks);
+		mu_run_test(test_queen_attacks);
+		mu_run_test(test_rook_attacks);
+		mu_run_test(test_bishop_attacks);
     return 0;
 }
 
