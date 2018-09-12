344ae811a7707196ffb7b35769b8bd90ca739da6
ThomOverhand <thom_overhand@hotmail.com>
2018-09-12 19:00:27 +0200

    WIP: Adding unit tests for piece attacks. Added G file to the clear_file (this broke knight movement).

--- src/bitboard.c
+++ src/bitboard.c
@@ -11,7 +11,7 @@ Bitboard clear_rank[RANK_LEN] = { 0xFFFFFFFFFFFFFF00ULL, 0xFFFFFFFFFFFF00FFULL,
 														0xFF00FFFFFFFFFFFFULL, 0x00FFFFFFFFFFFFFFULL };                          
 
 Bitboard clear_file[FILE_LEN] = { 0xFEFEFEFEFEFEFEFEULL, 0xFDFDFDFDFDFDFDFDULL, 0xFBFBFBFBFBFBFBFBULL, 0x0,
-																		0x0, 0x0, 0x0, 0x7F7F7F7F7F7F7F7FULL};
+																		0x0, 0x0, 0xFDFDFDFDFDFDFDFDULL, 0x7F7F7F7F7F7F7F7FULL};
 
 Bitboard get(Bitboard b, int square) {
     return (b & (1ULL << square));
