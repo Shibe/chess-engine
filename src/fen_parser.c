a9fbc6fdb8440e3894d0a9e70277579b8478d87a
ThomOverhand <thom_overhand@hotmail.com>
2018-09-17 15:40:29 +0200

    Refactor parsing fen

--- src/fen_parser.c
+++ src/fen_parser.c
@@ -8,91 +8,75 @@
 #include "chessboard.h"
 #include "fen_parser.h"
 
-regex_t FILE_REGEX;
 
 int parse(char *fen) {
     Chessboard *chessboard = initialise_chessboard();
     clear_chessboard(chessboard);
 
-    // ignore case because prnbqk and PRNBQK both need to match
-    int reg_error = regcomp(&FILE_REGEX, "([prnbqk]|[[:digit:]])", REG_ICASE | REG_EXTENDED);
-    if (reg_error) {
-        puts("Couldn't compile regex\n");
-        return 1;
+    int current_rank = RANK_8; 
+    int current_file = FILE_A;
+    
+    for(; fen[0] != ' ' && fen[0] != '\0'; fen++) {
+        char token = fen[0];
+        if (token == '/') {
+            current_rank--;
+            current_file = FILE_A;
+        } else if (isdigit(token)) {
+            current_file += atoi(&token);
+        } else {
+            Bitboard location = 1ULL << (current_rank * FILE_LEN + current_file);
+            int error = token_to_chessboard(token, location, chessboard);
+            if (error) {
+                printf("Invalid token %c", token);
+                return error;
+            }
+            current_file++;
+        }
     }
-
-    char *rank, *str;
-    str = strdup(fen);
-    for(int curr_rank = RANK_8; curr_rank >= RANK_1; curr_rank--) {
-        rank = strsep(&str, "/");
-        parse_rank_token(rank, curr_rank, chessboard);
-    }
-
     update_chessboard(chessboard);
     print_board(chessboard->all_pieces);
-    free(str);
     return 0;
 }
 
-int parse_rank_token(char rank[], int current_rank, Chessboard *board) {
-    int file = 0;
-    for (int i = 0; rank[i] != '\0'; i++) {
-        char c = rank[i];
-        int reg_error = regexec(&FILE_REGEX, &c, 0, NULL, 0);
-        if (reg_error) {
-            if (reg_error == REG_NOMATCH) {
-                printf("'%s' doesn't match \n", &c);
-            }
-            return 1;
-        }
-        if (isdigit(c)) {
-            file = file + atoi(&c);
-        } else {
-            Bitboard location = 1ULL << (current_rank * 8 + file);
-            token_to_chessboard(c, location, board);
-            file++;
-        }
-    }
-    return 0;
-}
-
-void token_to_chessboard(char token, Bitboard location, Chessboard *chessboard) {
+int token_to_chessboard(char token, Bitboard location, Chessboard *chessboard) {
     switch(token) {
         case 'P':
             chessboard->white_pawns |= location;
-            return;
+            return 0;
         case 'R':
             chessboard->white_rooks |= location;
-            return;
+            return 0;
         case 'N':
             chessboard->white_knights |= location;
-            return;
+            return 0;
         case 'B':
             chessboard->white_bishops |= location;
-            return;
+            return 0;
         case 'Q':
             chessboard->white_queens |= location;
-            return;
+            return 0;
         case 'K':
             chessboard->white_king |= location;
-            return;
+            return 0;
         case 'p':
             chessboard->black_pawns |= location;
-            return;
+            return 0;
         case 'r':
             chessboard->black_rooks |= location;
-            return;
+            return 0;
         case 'n':
             chessboard->black_knights |= location;
-            return;
+            return 0;
         case 'b':
             chessboard->black_bishops |= location;
-            return;
+            return 0;
         case 'q':
             chessboard->black_queens |= location;
-            return;
+            return 0;
         case 'k':
             chessboard->black_king |= location;
-            return;
+            return 0;
+        default:
+            return 1;
     }
 }
--- src/fen_parser.h
+++ src/fen_parser.h
@@ -1,5 +1,4 @@
 #include "bitboard.h"
 
-int parse_rank_token(char rank[], int rank_count, Chessboard *board);
 int parse(char* fen);
-void token_to_chessboard(char token, Bitboard location, Chessboard *chessboard);
+int token_to_chessboard(char token, Bitboard location, Chessboard *chessboard);
